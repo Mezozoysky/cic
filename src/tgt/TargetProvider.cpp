9d02752269a7b9e932ab5d7f921718828f079bf1
Stanislav Demyanovich <mezozoysky@gmail.com>
2016-06-26 20:32:37 +0600

    Remove SAX parser from TargetProvider.

--- src/tgt/TargetProvider.cpp
+++ src/tgt/TargetProvider.cpp
@@ -8,8 +8,8 @@
 
 #include <CICheck/tgt/TargetProvider.hpp>
 #include <CICheck/tgt/Target.hpp>
-#include <Poco/SAX/SAXParser.h>
-#include <Poco/SAX/ContentHandler.h>
+//#include <Poco/SAX/SAXParser.h>
+//#include <Poco/SAX/ContentHandler.h>
 #include <iostream>
 #include <sstream>
 #include <fmt/format.h>
@@ -18,114 +18,6 @@ using namespace fmt::literals;
 
 namespace cic {
 namespace tgt {
-
-class DeclReader
-: public Poco::XML::ContentHandler
-{
-public:
-    DeclReader()
-    : mLocator{ nullptr }
-    {
-    }
-
-    virtual void setDocumentLocator( const Poco::XML::Locator* locator ) override
-    {
-        mLocator = locator;
-    }
-
-    virtual void startDocument() override
-    {
-        fmt::print( "START DOCUMENT at [{}]\n", getLocation() );
-    }
-
-    virtual void endDocument() override
-    {
-        fmt::print( "END DOCUMENT at [{}]\n", getLocation() );
-    }
-
-	virtual void startElement(
-					  const Poco::XML::XMLString& uri
-					  , const Poco::XML::XMLString& localName
-					  , const Poco::XML::XMLString& qname
-					  , const Poco::XML::Attributes& attrList
-	) override
-    {
-        fmt::print( "START ELEMENT '{1}' at [{0}]\n", getLocation(), localName );
-        if ( localName == "targets" )
-        {
-            if ( mIsInTargetsList )
-            {
-                fmt::print( stderr, "[error] new <targets> element inside another one" );
-            }
-            mIsInTargetsList = true;
-        }
-    }
-
-	virtual void endElement(
-					const Poco::XML::XMLString& uri
-					, const Poco::XML::XMLString& localName
-					, const Poco::XML::XMLString& qname
-	) override
-	{
-        fmt::print( "END ELEMENT '{1}' at [{0}]\n", getLocation(), localName );
-    }
-
-	virtual void characters( const Poco::XML::XMLChar ch[], int start, int length ) override
-	{
-        fmt::print( "CHARACTERS at [{}]\n", getLocation() );
-        fmt::print( "\tline: '{}'\n", std::string( ch + start, length ) );
-	}
-
-	virtual void ignorableWhitespace( const Poco::XML::XMLChar ch[], int start, int length ) override
-	{
-        fmt::print( "IGNORABLE WS at [{}]\n", getLocation() );
-        fmt::print( "\tline: '{}'\n", std::string( ch + start, length ) );
-	}
-
-	virtual void processingInstruction( const Poco::XML::XMLString& target, const Poco::XML::XMLString& data ) override
-	{
-        fmt::print( "PROCESSING INSTUCTION at [{}]\n", getLocation() );
-    }
-
-	virtual void startPrefixMapping( const Poco::XML::XMLString& prefix, const Poco::XML::XMLString& uri ) override
-	{
-        fmt::print( "START PREFIX MAPPING '{1}' at [{0}]\n", getLocation(), prefix );
-    }
-
-	virtual void endPrefixMapping( const Poco::XML::XMLString& prefix ) override
-	{
-        fmt::print( "END PREFIX MAPPING '{1}' at [{0}]\n", getLocation(), prefix );
-	}
-
-	virtual void skippedEntity(const Poco::XML::XMLString& name) override
-	{
-        fmt::print( "SKIPPED ENTITY '{1}' at [{0}]\n", getLocation(), name );
-	}
-
-
-protected:
-	std::string getLocation()
-	{
-        std::string location;
-        if ( mLocator )
-        {
-            location = "{0}, line {1}, col {2}"_format(
-                                                    mLocator->getSystemId()
-                                                    , mLocator->getLineNumber()
-                                                    , mLocator->getColumnNumber()
-                                                    );
-		}
-		else
-		{
-			location = "location unknown";
-		}
-		return ( location );
-	}
-
-private:
-    const Poco::XML::Locator* mLocator;
-    bool mIsInTargetsList;
-};
 
 //Target::Target() noexcept
 //: mConfig{ nullptr }
@@ -183,23 +75,12 @@ void TargetProvider::reloadDecls()
 
     //TODO: load all the declarations avilable by given paths
 
-	DeclReader declReader;
-	Poco::XML::SAXParser parser;
-	parser.setContentHandler( &declReader );
-
 	for ( std::size_t i{ 0 }; i < mDeclPaths.size(); ++i )
     {
         Poco::Path path{ mDeclPaths[ i ] };
         std::cout << "targets file: " << path.toString() << std::endl;
         //            Poco::FileInputStream istr{ tgtsFilePath.toString() };
 
-        try
-        {
-            parser.parse( path.toString() );
-        } catch( Poco::Exception& e )
-        {
-            std::cerr << "[error] " << e.displayText() << std::endl;
-        }
     }
 }
 
