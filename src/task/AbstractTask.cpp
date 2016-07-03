09e2fe7bca344a219e07f788490809121a65a9dc
Stanislav Demyanovich <mezozoysky@gmail.com>
2016-07-03 14:58:41 +0600

    AbstractTask draft.

---
+++ src/task/AbstractTask.cpp
@@ -0,0 +1,17 @@
+//
+//  AbstractTask.cpp
+//  CICheck
+//
+//  Created by Stanislav Demyanovich on 01.07.16.
+//
+//
+
+#include <CICheck/task/AbstractTask.hpp>
+
+namespace cic
+{
+namespace task
+{
+
+} // namespace task
+} // namespace cic
